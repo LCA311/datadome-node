0da4292798a95c3b0a7c92823d794f4c9bd8e2d0
lca311 <spitzer-webdesign@outlook.de>
2018-06-08 19:30:18 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const http = require('https')
 const api = require('./apiModule')
 
 let server = http.createServer()
@@ -6,7 +6,7 @@ let jsonList = []
 
 api(jsonList, () => {
     console.log("Finished synchronisation")
-    server.listen(80, "0.0.0.0")
+    server.listen(443, "0.0.0.0")
 })
 
 server.on('request', (request, response) => {
